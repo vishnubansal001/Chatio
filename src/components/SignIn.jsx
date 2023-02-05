8eca6f046715e8a1ab4e620f257230dbdd496803
vishnubansal001 <vishnu1549.be21@chitkara.edu.in>
2023-02-05 21:24:08 +0530

    changed the email part in the sign in page to the phone number part

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -8,10 +8,10 @@ import { toast } from "react-toastify";
 export default function SignIn() {
   const [showPassword, setShowPassword] = useState(false);
   const [formData, setFormData] = useState({
-    email: "",
+    number: "",
     password: "",
   });
-  const { email, password } = formData;
+  const { number, password } = formData;
   const navigate = useNavigate();
   function onChange(e) {
     setFormData((prevState) => ({
@@ -25,7 +25,7 @@ export default function SignIn() {
       const auth = getAuth();
       const userCredential = await signInWithEmailAndPassword(
         auth,
-        email,
+        number,
         password
       );
       if (userCredential.user) {
@@ -49,11 +49,11 @@ export default function SignIn() {
         <div className="w-full md:w-[67%] lg:w-[40%] lg:ml-20">
           <form onSubmit={onSubmit}>
             <input
-              type="email"
-              id="email"
-              value={email}
+              type="number"
+              id="number"
+              value={number}
               onChange={onChange}
-              placeholder="Email address"
+              placeholder="Enter Number"
               className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out"
             />
             <div className="relative mb-6">
